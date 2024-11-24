edade17efbd2217e2d9d641900f461672ed5a652
TristanBourbigot <tristanbourbigot@gmail.com>
2024-11-25 00:00:50 +0100

    👌 IMPROVE: improve websocket

--- utils/websocket.js
+++ utils/websocket.js
@@ -2,7 +2,7 @@ import { Server } from 'socket.io';
 import { authSocket } from '../auth/index.js';
 
 class Websocket {
-    channels = ["general"];
+    channels = [];
     instance = null;
     io = null;
 
@@ -18,40 +18,42 @@ class Websocket {
           methods: ["GET", "POST"],
         },
       });
+      
       this.init();
       Websocket.instance = this;
-    }
-
-    init(){
-      for (let channel of this.channels) {
-        this.io.of(channel).on("connection", (socket) => {
-            this.messageHandler(socket, channel);
-
-            console.log(`new room opened: ${channel}`);
-        });
-    }
     }
     
     messageHandler(socket, channel)
     {
-        socket.on("message", (msg) =>
+      console.log(`new connection on ${channel}`);
+        socket.on("chat message", (msg) =>
         {
-            console.log(`Received ${msg}`);
-
-            this.of(channel).emit("message", `${socket.user.dataValues.username} : ${msg}`);
+            this.io.of(channel).emit("chat message", msg);
         });
     }
 
-    openChannel()
+    
+    init()
     {
-        this.io.use(authSocket);
-        const channel = `channel-${this.channels.length + 1}`;
-        this.channels.push(channel);
+        for (let channel of this.channels) {
+            this.io.of(channel).use(authSocket);
+            this.io.of(channel).on("connection", (socket) => {
+                this.messageHandler(socket, channel);
 
+                console.log(`new channel opened: ${channel}`);
+            });
+        }
+    }
+
+    openChannel(id)
+    {
+        const channel = `channel-${id}`;
+        this.channels.indexOf(channel) === -1 ? this.channels.push(channel) : console.log("This channel already exists");
+        this.io.of(channel).use(authSocket);
         this.io.of(channel).on("connection", (socket) => {
             this.messageHandler(socket, channel);
 
-            console.log(`new room opened: ${channel}`);
+            console.log(`Opened channel: ${channel}`);
         });
 
         return channel;
@@ -60,30 +62,3 @@ class Websocket {
 }
 
 export const websocket = new Websocket(3001);
-
-// export function initSocket(server) {
-//   const io = new Server(server, {
-//     cors: {
-//       origin: "*",
-//       methods: ["GET", "POST"],
-//     },
-//   });
-
-  
-//   io.use(authSocket);
-
-  
-//   io.on('connection', (socket) => {
-//     console.log('a user connected: ' + socket.user.dataValues.username); 
-
-//     socket.on('chat message', (msg) => {
-//         io.emit('chat message', `${socket.user.dataValues.username} : ${msg}`); 
-//     });
-
-//     socket.on('disconnect', () => {
-//         console.log('user disconnected');
-//     });
-//   });
-
-//   return io;
-// }
